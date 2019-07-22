06e12638d3c8279d7d6261c75174a237027e431c
sbaner2 <Sbaner25>
2019-07-22 00:40:32 -0500

    Added JavaPassByValue example

--- coding_examples/src/main/java/com/example/test/JavaPassByValue.java
+++ coding_examples/src/main/java/com/example/test/JavaPassByValue.java
@@ -3,8 +3,8 @@ package com.example.test;
 /**
  * We pass an int to the function “change()” and as a result the change in the value of
  * that integer is not reflected in the main method. Like C/C++,
- * Java creates a copy of the variable being passed
- * in the method and then do the manipulations. Hence the change is not reflected in the main method.
+ * Java creates a COPY of the variable being passed in the method and then do the manipulations.
+ * Hence the change is not reflected in the main method.
  */
 public class JavaPassByValue {
 
@@ -12,10 +12,11 @@ public class JavaPassByValue {
     {
         int x = 5;
         change(x);
-        System.out.println(x);
+        System.out.println("No change in the value of X as it was passed by value : " + x);
     }
     public static void change(int x)
     {
+        System.out.println("passed in value of x: " + x);
         x = 10;
     }
 }
